7e4014b0249a685ce0fb6631c9ee8512c0a3e56e
Filippo Pisello <66630817+FilippoPisello@users.noreply.github.com>
2024-05-20 12:51:17 +0200

    Remove unused query (#39)

--- serie_a_db/queries/definitions/dm_season_staging.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE dm_season_staging (
-    year_start INT NOT NULL CHECK (year_start BETWEEN 1980 AND 2050),
-    code_serie_a_api INT NOT NULL,
-    status STR NOT NULL CHECK (status IN ("completed", "ongoing", "upcoming"))
-);
