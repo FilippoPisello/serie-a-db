85cd4b82ca28690478741a152637d031be42accb
Filippo <filippo.pisello@live.it>
2024-07-07 14:32:03 +0200

    Merge main

--- serie_a_db/db/schema.py
+++ serie_a_db/db/schema.py
@@ -8,11 +8,14 @@ from serie_a_db.data_extraction.table_specific_extractors.st_match_day import (
 )
 from serie_a_db.db.table import DbTable
 from serie_a_db.db.table import StagingTable as St
-from serie_a_db.db.table import WarehouseTable as Pt
+from serie_a_db.db.table import WarehouseTable as Wt
 
 TABLES: dict[str, DbTable] = {
-    "dm_season": Pt.from_file("dm_season"),
-    "dm_match_day": Pt.from_file("dm_match_day"),
+    # Warehouse tables
+    "dm_season": Wt.from_file("dm_season"),
+    "dm_match_day": Wt.from_file("dm_match_day"),
+    "dm_team": Wt.from_file("dm_team"),
+    # Staging tables
     "st_match_day": St.from_file("st_match_day", scrape_match_day_data),
     "st_match": St.from_file("st_match", scrape_match_data),
 }
