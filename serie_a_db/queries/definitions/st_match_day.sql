126261a56c1a02c18d7791e95e24233e2495a23b
Filippo <filippo.pisello@live.it>
2024-07-28 15:04:14 +0200

    Fix query

--- serie_a_db/queries/definitions/st_match_day.sql
+++ serie_a_db/queries/definitions/st_match_day.sql
@@ -3,6 +3,6 @@ CREATE TABLE st_match_day (
     season_year_start INT NOT NULL CHECK (season_year_start BETWEEN 1980 AND 2050),
     code_serie_a_api INT NOT NULL,
     number INT NOT NULL CHECK (number BETWEEN 1 AND 38),
-    status STR NOT NULL CHECK (status IN ("completed", "ongoing", "upcoming"))
+    status STR NOT NULL CHECK (status IN ("completed", "ongoing", "upcoming")),
     PRIMARY KEY (season_year_start, number)
 );
