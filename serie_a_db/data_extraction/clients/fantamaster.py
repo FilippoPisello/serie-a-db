48a5ed7b63c6090a407bd84a9d7230c1d7a15293
Filippo <filippo.pisello@live.it>
2024-08-12 14:08:17 +0200

    Run linters

--- serie_a_db/data_extraction/clients/fantamaster.py
+++ serie_a_db/data_extraction/clients/fantamaster.py
@@ -10,6 +10,7 @@ class FantamasterWebsite:
 
     @classmethod
     def get_players(cls) -> list[dict]:
+        """Return a list of players data, each represented as a dict."""
         resp = requests.get(cls.ROOT_API + "/playersstats/", timeout=5)
         resp.raise_for_status()
         return resp.json()["players"]
